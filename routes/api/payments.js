afef1da92b8bf9fbcd2a17061ee90c9562d0c228
Lawe Sosah <sosahlawe@gmail.com>
2024-03-19 09:57:29 +0200

    local changes

--- routes/api/payments.js
+++ routes/api/payments.js
@@ -11,6 +11,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 
 router.post('/create-checkout-session', async (req, res) => {
     const { amount, description, success_url, cancel_url, imageUrl, houseId, startDate, endDate, nights } = req.body;
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [
@@ -38,14 +39,10 @@ router.post('/create-checkout-session', async (req, res) => {
       client_reference_id: houseId, // Pass the houseId here
 
     });
-    const house = await House.findOneAndUpdate(
-      { orderId }, // Update query
-      { $push: { orders: { customerId, customerEmail, amountPaid, startDate, endDate, nights } } }, // Add the order details to the orders array
-      { new: true } // Return the updated document
-    );
+ 
 
     // Log the updated house document
-    console.log('Updated House:', house);
+    // console.log('Updated House:', house);
     
     // Respond with success status
     res.sendStatus(200);
